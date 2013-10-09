91337bce2f816aad46bbe935778bd05aed5611b2
mdp <mdp@yahoo-inc.com>
2013-10-08 20:55:23 -0700

    More tests

--- test/consumer_test.js
+++ test/consumer_test.js
@@ -1,15 +1,52 @@
 var assert = require("assert"),
   Consumer = require("../lib/consumer").Consumer,
-  mockCredentials = require("./mocks/credentials").valid();
+  apiMethods = require("../lib/api_methods"),
+  mockCredentials = require("./mocks/credentials").valid(),
+  mockExpiredCredentials = require("./mocks/credentials").expired();
 
 describe('Consumer', function(){
   describe('api methods', function(){
     var consumer = new Consumer(mockCredentials, {});
     it('should correctly add methods from the api methods file', function(){
-      assert(typeof consumer.account_balance === 'function');
-      assert(typeof consumer.sell === 'function');
-      assert(typeof consumer.buy === 'function');
-      assert(typeof consumer.user_list === 'function');
+      for (var name in apiMethods) {
+        if (apiMethods.hasOwnProperty(name)) {
+          assert(typeof consumer[name] === 'function');
+        }
+      }
+    })
+  })
+
+  describe('working with credentials', function(){
+    it('should refresh expired tokens on action', function(done){
+      var client = {
+          refreshAccessTokens: function(token, callback) {
+            assert(true);
+            done();
+          },
+          adapter: function () {
+            assert(false, "Didn't refresh expired token")
+            done();
+          }
+        },
+        consumer = new Consumer(mockExpiredCredentials, client);
+      consumer.get('api_method/me');
+    })
+
+    it('should fire a refresh token event', function(done){
+      var client = {
+          refreshAccessTokens: function(token, callback) {
+            assert(true);
+            callback(null, mockCredentials);
+          },
+          adapter: function () {
+          }
+        },
+        consumer = new Consumer(mockExpiredCredentials, client);
+      consumer.on('updatedCredentials', function (creds) {
+        assert.equal(creds, mockCredentials)
+        done();
+      });
+      consumer.get('api_method/me');
     })
   })
 })
